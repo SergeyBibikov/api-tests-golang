4c67dff17b3f3d5ec9b11411ebd16bd4171bfb04
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-04-24 17:53:27 +0300

    Test launch script

--- suites.go
+++ suites.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/stretchr/testify/suite"
 
 	"github.com/go-resty/resty/v2"
@@ -13,8 +15,16 @@ type BaseSuite struct {
 
 func (s *BaseSuite) SetupSuite() {
 	s.client = resty.New().SetBaseURL("http://localhost:8080")
-	_, err := s.client.R().Get("/ready")
+	var err error
+	var r *resty.Response
+	for i := 0; i < 20; i++ {
+		r, err = s.client.R().Get("/ready")
+		if r.StatusCode() == 200 {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 	if err != nil {
-		s.T().Errorf("The service is not ready %s", err)
+		s.T().Errorf("The service was not available for 2 seconds %s", err)
 	}
 }
