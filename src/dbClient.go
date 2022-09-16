28b9283e3bb71de65690953b674e3c5d2fa65dbd
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-09-16 13:36:24 +0300

    WIP GetUser

--- src/dbClient.go
+++ src/dbClient.go
@@ -3,6 +3,7 @@ package src
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -12,11 +13,24 @@ type DbClient struct {
 	conn *pgx.Conn
 }
 
-func NewDbClient(ctx context.Context, dbPass string) (*DbClient, error) {
+type User struct {
+	Id, RoleId, FavPlayerId  int
+	Username, Email, FavTeam string
+}
+
+func NewDbClient() (*DbClient, error) {
+	ctx := context.TODO()
+	dbPass := os.Getenv("DBPass")
 	dbUrl := fmt.Sprintf("postgres://postgres:%s@localhost:5432/postgres", dbPass)
 	conn, err := pgx.Connect(ctx, dbUrl)
 	if err != nil {
 		return nil, err
 	}
 	return &DbClient{ctx, conn}, nil
+}
+
+func (d *DbClient) GetUser(filters map[string]string) User {
+	var u User
+	d.conn.Query()
+	return u
 }
